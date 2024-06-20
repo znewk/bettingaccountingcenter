2cffcfbd92d84e063e08ba1003affde6c5a98045
znewk <zane.css34@gmail.com>
2024-06-20 10:30:26 +0500

    fix: packages update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,23 +1,22 @@
-import Header from "@/components/Header";
-import styles from "../../styles/main.module.css"
+"use client"
+import styles from '../styles/main.module.css'
 import { Metadata } from 'next';
-
+import Header from "@/components/Header";
 
 export const metadata: Metadata = {
     title: 'Центр учёта ставок',
 };
 
-const Unauthorized: React.FC = () => {
-    
+const NotFound: React.FC = () => {
     return (
         <div>
-            
             <Header/>
+
             <div className={styles.container}>
-                <h1>Unauthorized</h1>
+                <h1>404 not found</h1>
             </div>
         </div>
     );
 }
 
-export default Unauthorized;
+export default NotFound;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Header from '@/components/Header';
 import styles from '../styles/main.module.css'
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Центр учёта ставок',
+};
 
 const Index: React.FC = () => {
   return (
